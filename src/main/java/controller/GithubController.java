a5292712866798c29789b73e3088ac96d5f1a9a6
murat_eksi <info@murateksi.com>
2019-05-17 15:05:44 +0200

    hotfix ping

--- src/main/java/controller/GithubController.java
+++ src/main/java/controller/GithubController.java
@@ -23,40 +23,41 @@ public class GithubController {
         String repo_name = payload.get("repository").getAsJsonObject().get("name").getAsString();
         Repository repository = Repository.getRepository(repo_name);
         User user = User.getUser(repository.getUser_uuid());
+        User admin = User.getUser(repository.getRepository_admin_uuid());
         String challenge_type = repository.getChallenge_type();
         String result = "";
-
-        if(challenge_type.equals("structure-diff")){
-            result = diffRepositories(repository.getRepository_git_url(), repository.getSolution_repository_git_url());
-        }
-
-        if (checkResult(result)){
-            repository.setRepository_status("success");
-            if(repository.getCanvas_assignment_uuid() != null) {
-                gradeAssignment(user, repository.getCanvas_course_uuid(), repository.getCanvas_assignment_uuid(), repository.getCanvas_student_uuid(), "pass");
+        if (!req.headers("X-GitHub-Event").equals("ping")) {
+            if (challenge_type.equals("structure-diff")) {
+                result = diffRepositories(repository.getRepository_git_url(), repository.getSolution_repository_git_url());
             }
-            Repository.updateRepository(repository);
 
-            SuccessEmail email = new SuccessEmail(repository.getUser_uuid(), repo_name);
-            email.sendMail();
-        }
-        else {
-            repository.setRepository_status("fail");
-            if(repository.getCanvas_assignment_uuid() != null) {
-                gradeAssignment(user, repository.getCanvas_course_uuid(), repository.getCanvas_assignment_uuid(), repository.getCanvas_student_uuid(), "fail");
+            if (checkResult(result)) {
+                repository.setRepository_status("success");
+                if (repository.getCanvas_assignment_uuid() != null) {
+                    gradeAssignment(admin, repository.getCanvas_course_uuid(), repository.getCanvas_assignment_uuid(), repository.getCanvas_student_uuid(), "pass");
+                }
+                Repository.updateRepository(repository);
+
+                SuccessEmail email = new SuccessEmail(repository.getUser_uuid(), repo_name);
+                email.sendMail();
+            } else {
+                repository.setRepository_status("fail");
+                if (repository.getCanvas_assignment_uuid() != null) {
+                    gradeAssignment(admin, repository.getCanvas_course_uuid(), repository.getCanvas_assignment_uuid(), repository.getCanvas_student_uuid(), "fail");
+                    System.out.println("lol");
+                }
+                Repository.updateRepository(repository);
+                FailEmail email = new FailEmail(repository.getUser_uuid(), repo_name, result);
+                email.sendMail();
             }
-            Repository.updateRepository(repository);
-            FailEmail email = new FailEmail(repository.getUser_uuid(), repo_name, result);
-            email.sendMail();
         }
-
         res.status(200);
         return "payload processed";
     }
 
-    private static boolean checkResult(String result){
-        for (String line: result.split("\n")) {
-            if(line.length() > 0) {
+    private static boolean checkResult(String result) {
+        for (String line : result.split("\n")) {
+            if (line.length() > 0) {
                 if (line.charAt(0) == '+' || line.charAt(0) == '-')
                     return false;
             }
